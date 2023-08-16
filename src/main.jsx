181ef996f1237892b4c91a741f3a81d9b4f28e89
Wilton Lira Martins <wiltonliramartins@gmail.com>
2023-08-16 00:44:54 -0300

    <Refactor>: Project refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<AuthProvider>
-				<Routes />
-			</AuthProvider>
+			<Routes />
 		</ThemeProvider>
 	</React.StrictMode>
 );
