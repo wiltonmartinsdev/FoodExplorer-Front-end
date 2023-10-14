0231dcc13323699581efc3b3c068fa3bff211be2
Wilton Lira Martins <wiltonliramartins@gmail.com>
2023-10-13 22:15:31 -0300

    <Refactor>: Refactoring project

--- src/routes/appUser.routes.jsx
+++ src/routes/appUser.routes.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 // Import of Pages
 import Home from "../Pages/CommonUser/Home";
 import DishInformation from "../Pages/CommonUser/DishInformation";
-import Payments from "../Pages/CommonUser/Payments";
+// import Payments from "../Pages/CommonUser/Payments";
 
 function AppUserRoutes() {
 	return (
